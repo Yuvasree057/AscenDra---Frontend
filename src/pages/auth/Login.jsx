15a94e58670332123c5836df6dbc38c3e431830f
yuvasreepadmanaban057 <yuvasreepadmanaban057@gmail.com>
2026-05-17 10:36:48 +0530

    Add files via upload

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -11,6 +11,7 @@ export default function Login() {
   const { login } = useAppContext();
   const [method, setMethod] = useState('email');
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [isForgotPassword, setIsForgotPassword] = useState(false);
